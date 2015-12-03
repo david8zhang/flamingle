a0db21b70fdfdf3df38ff80face3333137d1b6c9
Helen Che <helenc@berkeley.edu>
2015-12-02 19:56:38 -0800

    Finish index

--- app/controllers/cods_controller.rb
+++ app/controllers/cods_controller.rb
@@ -1,9 +1,11 @@
 class CodsController < ApplicationController
   def index
     @cods = Cod.all
+    @countries = Country.all
   end
 
   def show
+    @cods = Cod.all
     @cod = Cod.find(params[:id])
     @countries = Country.all
   end
