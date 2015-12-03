77d1209f879babc9309e4c15d27103d3d75ba6d5
David Zhang <david8zhang@berkeley.edu>
2015-12-03 00:29:02 -0800

    did stuff

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,21 +5,10 @@ class CountriesController < ApplicationController
 
   # Show a list of charities for the country with the given cod
   def show
-      @country = Country.find(params[:country_id])
-      @cod = Cod.find(params[:cod_id])
-      @charities = Charity.where(:cod_id => params[:cod_id], :country_id => params[:country_id])
-      @donations = []
-      @charities.each do |c|
-          @donations << calc_total_donation(c.donations)
-      end
-  end
-
-  # Calculate the total donations
-  def calc_total_donation(donations)
-      total = 0
-      donations.each do |donation|
-          total = total + donation.amount
-      end
-      return total
+      @countryid = params[:cod_id, :country_id].country_id
+      @codid = params[:cod_id, :country_id].cod_id
+    #   @country = Country.find(params[:country_id])
+    #   @cod = Cod.find(params[:cod_id])
+    #   @charities = Charity.where(:cod_id => params[:cod_id], :country_id => params[:country_id])
   end
 end
