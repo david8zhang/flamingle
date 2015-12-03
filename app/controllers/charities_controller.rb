2d1bf97688adeeec8674e516ff899add4dd59c0b
David Zhang <david8zhang@berkeley.edu>
2015-12-03 14:28:24 -0800

    added some things

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -10,7 +10,7 @@ class CharitiesController < ApplicationController
         description = params[:charity][:description]
         cod_id = params[:charity][:cod_id]
 		country_id = params[:charity][:country_id]
-        new_charity = current_user.charities.create(name: name, description: description, cod_id: cod_id, country_id: country_id)
+        new_charity = current_user.charities.build(name: name, description: description, cod_id: cod_id, country_id: country_id)
         if !new_charity.valid?
             flash[:error] = new_charity.errors.full_messages.to_sentence
             redirect_to :back
